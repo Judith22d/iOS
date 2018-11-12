63e38e021e76a033cfb7dd9ff71390ff97cf1a98
Magdalena Judith Vargas Flores <judith@macbook-pro-de-ios-2.local>
2018-11-12 13:39:04 -0600

    Initial commit

---
+++ ProyectoiOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ProyectoiOS
+//
+//  Created by Magdalena Judith Vargas Flores on 05/11/18.
+//  Copyright © 2018 UANL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //asdas
+
+
+}
+
